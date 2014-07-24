789e2d355dc8d769ffdfdc05b60d4825aca55a0b
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2014-07-24 10:47:56 +0200

    Added the news_url token

--- config/config.php
+++ config/config.php
@@ -15,13 +15,13 @@
 $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['news_newsletter']['news_newsletter_default'] = array
 (
      'recipients'           => array('admin_email', 'recipient_email'),
-     'email_subject'        => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'recipient_email'),
-     'email_text'           => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'recipient_email'),
-     'email_html'           => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'recipient_email'),
-     'file_name'            => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'recipient_email'),
-     'file_content'         => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'recipient_email'),
-     'email_recipient_cc'   => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'recipient_email'),
-     'email_recipient_bcc'  => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'recipient_email'),
-     'email_replyTo'        => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'recipient_email'),
+     'email_subject'        => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_text'           => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_html'           => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'file_name'            => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'file_content'         => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_recipient_cc'   => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_recipient_bcc'  => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_replyTo'        => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
      'attachment_tokens'    => array(),
 );
--- dca/tl_news.php
+++ dca/tl_news.php
@@ -124,6 +124,10 @@ class tl_news_newsletter extends tl_news
             }
         }
 
+        // Generate news URL
+        $objPage = \PageModel::findWithDetails($objNews->getRelated('pid')->jumpTo);
+        $arrTokens['news_url'] = ($objPage->rootUseSSL ? 'https://' : 'http://') . ($objPage->domain ?: \Environment::get('host')) . TL_PATH . '/' . $objPage->getFrontendUrl((($GLOBALS['TL_CONFIG']['useAutoItem'] && !$GLOBALS['TL_CONFIG']['disableAlias']) ?  '/' : '/items/') . ((!$GLOBALS['TL_CONFIG']['disableAlias'] && $objNews->alias != '') ? $objNews->alias : $objNews->id));
+
         // Administrator e-mail
         $arrTokens['admin_email'] = $GLOBALS['TL_ADMIN_EMAIL'];
 
