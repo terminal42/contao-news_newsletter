321b1163b4c0707f3b6b6dd4c4ae51f370d0143b
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2014-08-11 11:05:21 +0200

    Added the German translation

---
+++ languages/de/tl_nc_notification.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ??? extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2008-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-???
+ */
+
+/**
+ * Notification types
+ */
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['news_newsletter']         = 'News Newsletter';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['news_newsletter_default'] = array('Standard', 'Dieser Benachrichtigungstyp kann für Newsletter bei Veröffentlichung eines News-Beitrags verwendet werden.');
---
+++ languages/de/tl_news_archive.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * ??? extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2008-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-???
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_news_archive']['newsletter']         = array('Newsletter aktivieren', 'Newsletter für Neuigkeiten aktivieren.');
+$GLOBALS['TL_LANG']['tl_news_archive']['newsletter_channel'] = array('Newsletter-Verteiler', 'Bitte wählen Sie hier den Verteiler.');
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_news_archive']['newsletter_legend'] = 'Newsletter-Einstellungen';
